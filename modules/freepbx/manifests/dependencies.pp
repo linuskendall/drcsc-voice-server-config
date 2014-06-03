e89856c653bce56b3639d52e2b39a88d4b1f14da
Linus Kendall <me@linuskendall.com>
2014-06-03 16:08:08 +0530

    Added mpg123 dependencies required by google tts

--- modules/freepbx/manifests/dependencies.pp
+++ modules/freepbx/manifests/dependencies.pp
@@ -45,9 +45,6 @@ class freepbx::dependencies {
   package { 'sox':
     ensure => 'installed',
   }
-  package { 'mpg123':
-    ensure => 'installed',
-  }
  
   package { 'newt-devel':
     ensure => 'installed',
@@ -89,4 +86,23 @@ class freepbx::dependencies {
   package { 'openssl-devel':
     ensure => 'installed',
   }
+
+  include wget
+  wget::fetch { 'rpmforge-release':
+    source => 'http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm',
+    cache_dir => '/tmp',
+    destination => '/root/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm',
+  }
+
+  exec { 'rpm_forge_install':
+    command => "rpm -ivh --replacepkgs /tmp/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm",
+    require => File['/root/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm'],
+  }
+
+  package { 'mpg123':
+    ensure => 'installed',
+    install_options => ['--enablerepo=rpmforge-extras'],
+    require => Exec['rpm_forge_install'],
+  }
+  
 }
